58f89455ce15a0bb2c41cc2cfb610de0366370ac
Lucas Fabiano <p000lfabiano@prservicos.com.br>
2018-06-25 08:48:39 -0300

    navbar para dark e alert quando salva a versao

--- src/app/versoes-app-detalhes/versoes-app-detalhes.component.ts
+++ src/app/versoes-app-detalhes/versoes-app-detalhes.component.ts
@@ -10,25 +10,28 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class VersoesAppDetalhesComponent implements OnInit {
 
-  versao: Versao = {versaoPk : {cdVersaoApp: '', cdSistemaOperacional:  0}};
+  versao: Versao = { versaoPk: { cdVersaoApp: '', cdSistemaOperacional: 0 } };
   versaoPk: {};
   tipoEdicao: string = 'editar';
-  habilitadoOptions = [{value:'S', text: 'Sim'}, {value:'N', text: 'Não'}];
-  sistemaOperacionalOptions = [{value:'1', text: 'Android'}, {value:'2', text: 'iOS'}];
+  habilitadoOptions = [{ value: 'S', text: 'Sim' }, { value: 'N', text: 'Não' }];
+  sistemaOperacionalOptions = [{ value: '1', text: 'Android' }, { value: '2', text: 'iOS' }];
+
+  alertSuccess = { show: false, mensagem: 'Versão salva com sucesso!' };
+  alertError = { show: false, mensagem: 'Mensagem de erro' };
 
   constructor(private router: Router, private route: ActivatedRoute, private versoesAtivasService: VersoesAtivasService) {
     this.route.params.subscribe(params => {
 
-      if (params.cdVersaoApp) {        
-        let versaoTemp: Versao = {versaoPk: {cdVersaoApp: params.cdVersaoApp, cdSistemaOperacional: params.cdSistemaOperacional}};
-        
+      if (params.cdVersaoApp) {
+        let versaoTemp: Versao = { versaoPk: { cdVersaoApp: params.cdVersaoApp, cdSistemaOperacional: params.cdSistemaOperacional } };
+
         this.versoesAtivasService.getVersao(versaoTemp).subscribe(data => {
           this.versao = data;
           this.versaoPk = Object.assign({}, data.versaoPk);
           this.tipoEdicao = 'editar';
         });
       } else {
-        this.versao = {versaoPk: {cdVersaoApp: '', cdSistemaOperacional: 0}};
+        this.versao = { versaoPk: { cdVersaoApp: '', cdSistemaOperacional: 0 } };
         this.tipoEdicao = 'novo';
       }
     })
@@ -46,14 +49,28 @@ export class VersoesAppDetalhesComponent implements OnInit {
   }
 
   public salvarVersao() {
+    this.alertSuccess.show = false;
+    this.alertError.show = false;
+
     //salva o valor do sistema operacional devido a modelagem do banco 
     this.versao.nomeSistemaOperacional = this.converterSO(this.versao.versaoPk.cdSistemaOperacional);
     if (this.tipoEdicao === 'novo') {
-      this.versoesAtivasService.salvarVersao(this.versao).subscribe(params => console.log(params));
+      this.versoesAtivasService.salvarVersao(this.versao).subscribe(data => this.alertSuccess.show = true,
+        errorResponse => {
+          this.alertError.mensagem = errorResponse.error.message;
+          this.alertError.show = true;
+        }
+      );
     } else {
-      this.versoesAtivasService.atualizarVersao(this.versaoPk, this.versao).subscribe(params => console.log(params));
+      this.versoesAtivasService.atualizarVersao(this.versaoPk, this.versao).subscribe(
+        () => this.alertSuccess.show = true,
+        errorResponse => {
+          this.alertError.mensagem = errorResponse.error.message;
+          this.alertError.show = true;
+        });
     }
   }
+
 
   public cancelarEditar() {
     this.router.navigate(['versoesAtivas']);
